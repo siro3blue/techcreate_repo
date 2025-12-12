9faab74bdd7869582ee97af1c7c497d40c136f65
siro3blue <gradjob_str@yahoo.com>
2025-12-12 17:01:25 +0800

    Create SchemaField.java

---
+++ Interview/src/main/java/com/example/Interview/SchemaField.java
@@ -0,0 +1,13 @@
+package com.example.Interview;
+
+public class SchemaField {
+	public String name;
+	public int start;
+	public int end;
+
+	public SchemaField(String name, int start, int end) {
+		this.name = name;
+		this.start = start;
+		this.end = end;
+	}
+}
